8cde819300c52aa19a7fe57e037cdd20cd96b8f0
InSeop Hwang <inseop.hwang@hotmail.com>
2023-04-29 23:43:56 +0900

    argumentation study

--- 4_Deep Learning/3_케라스 창시자에게 배우는 딥러닝/8.2_소규모 데이터셋에서 밑바닥부터 컨브넷 훈련하기.py
+++ 4_Deep Learning/3_케라스 창시자에게 배우는 딥러닝/8.2_소규모 데이터셋에서 밑바닥부터 컨브넷 훈련하기.py
@@ -4,3 +4,27 @@
 3) 97.5% 성능: 사전 훈련된 네트워크로 특성을 추출
 4) 98.5% 성능: 사전 훈련된 네트워크를 세밀하게 튜닝
 '''
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+def func_make_model():
+
+    inputs = keras.Input(shape=(180, 180, 3))
+
+    x = layers.Rescaling(1./255)(inputs)
+    x = layers.Conv2D(filters=32, kernel_size=3, actionation='rele')(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation='relu')(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=128, kernel_size=3, activation='relu')(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=256, kernel_size=3, activation='relu')(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=256, kernel_size=3, activation='relu')(x)
+    x = layers.Flatten()(x)
+
+    outputs = layers.Dense(1, activation='sigmoid')(x)
+
+    return keras.Model(inputs, outputs)
