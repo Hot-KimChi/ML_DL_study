68b80074090be03573d421a5d8d6d9c52580d840
InSeop Hwang <inseop.hwang@hotmail.com>
2023-05-01 23:33:30 +0900

    update datasetII

--- 4_Deep Learning/3_케라스 창시자에게 배우는 딥러닝/8.2_소규모 데이터셋에서 밑바닥부터 컨브넷 훈련하기.py
+++ 4_Deep Learning/3_케라스 창시자에게 배우는 딥러닝/8.2_소규모 데이터셋에서 밑바닥부터 컨브넷 훈련하기.py
@@ -1,10 +1,10 @@
 '''
+---------------------------------------------------------------------------
 1) 70% 성능: 작은 컨브넷 훈련
 2) 80~85% 성능: 과대적합을 줄이기 위한 강력한 방법인 데이터증식
 3) 97.5% 성능: 사전 훈련된 네트워크로 특성을 추출
 4) 98.5% 성능: 사전 훈련된 네트워크를 세밀하게 튜닝
 '''
-
 from tensorflow import keras
 from keras import layers
 import tensorflow as tf
@@ -35,6 +35,38 @@ print(model.summary())
 
 
 '''
+---------------------------------------------------------------------------
+make dataset from directory
+'''
+import os, shutil, pathlib
+
+org_dir = pathlib.Path('D:\PycharmProjects\dogs-vs-cats\train')
+new_dir = pathlib.Path('D:\PycharmProjects\dogs-vs-cats\dataset')
+
+def func_make_subset(subset_name, start_index, end_index):
+    for category in ('cat', 'dog'):
+        dir = new_dir / subset_name / category
+        os.makedirs(dir)
+
+        fnames = [f'{category}.{i}.jpg'
+                  for i in range(start_index, end_index)]
+
+        for fname in fnames:
+            print(fname)
+            shutil.copyfile(src=org_dir/fname, dst=dir/fname)
+
+
+func_make_subset('train', start_index=0, end_index=1000)
+func_make_subset('validation', start_index=1000, end_index=1500)
+func_make_subset('test', start_index=1500, end_index=2500)
+
+
+
+
+
+
+
+'''
 모델 훈련 설정하기
 '''
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
@@ -49,9 +81,13 @@ model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accurac
 5) 배치로 묶는다.
 --> image_dataset_from_directory 로 위의 단계 자돟화 가능
 '''
+train_dir = 'D:\PycharmProjects\dogs-vs-cats\train'
+test_dir = 'D:\PycharmProjects\dogs-vs-cats\test'
+
 from keras.utils import image_dataset_from_directory
 
 
 train_ds = image_dataset_from_directory(
+
 
 )
