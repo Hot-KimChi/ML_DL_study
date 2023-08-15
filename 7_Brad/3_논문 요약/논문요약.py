1929dba556c52bfb70818f07b9a910031c61ae4c
InSeop Hwang <inseop.hwang@hotmail.com>
2023-08-15 23:46:54 +0900

    update summarization of paper code.

--- 7_Brad/3_논문 요약/논문요약.py
+++ 7_Brad/3_논문 요약/논문요약.py
@@ -1,2 +1,11 @@
+"""
+OpenAPI를 이용한 논문, 깃, 글 요약 및 번역
+"""
+
+
+
+## API 키 발급
+## RapidAPI : https://rapidapi.com/developer/new
+## 네이버 : https://developers.naver.com/apps/#/register?api=ppg_n2mt
 
 rapidapi_key = 'c149679a1amsh76701cf7091cd86p1de503jsn19f933ebcc7b'
