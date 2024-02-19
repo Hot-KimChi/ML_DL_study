a10ce362880d336a10055dff0164fca9be32c980
InSeop Hwang <inseop.hwang@hotmail.com>
2024-02-19 23:45:22 +0900

    update dependancy

--- 8_Keras_web/3_EDSR/EDSR_20240214.py
+++ 8_Keras_web/3_EDSR/EDSR_20240214.py
@@ -1,5 +1,11 @@
+## import and dependancy
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
 
+from tensorflow import _keras
+from tensorflow.keras import layers
+
+AUTOTUNE = tf.data.AUTOTUNE
 
