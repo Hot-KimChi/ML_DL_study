0402ce651b59009e65a49aaae03410ca46993d20
InSeop Hwang <inseop.hwang@hotmail.com>
2024-02-20 21:41:17 +0900

    GPU and CUDA setup

--- 8_Keras_web/3_EDSR/EDSR_20240214.py
+++ 8_Keras_web/3_EDSR/EDSR_20240214.py
@@ -9,3 +9,22 @@ from tensorflow.keras import layers
 
 AUTOTUNE = tf.data.AUTOTUNE
 
+
+## GPU check and CUDA to use
+## Tensorflow 선택할 경우, 
+# GPU를 사용하도록 설정
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+# GPU를 사용하도록 설정
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        # GPU 메모리 성장을 허용(메모리 제한을 조절 가능: memery - 6GB로 제한)
+        tf.config.set_logical_device_configuration(gpus[0],
+            [tf.config.LogicalDeviceConfiguration(memory_limit=1024 * 6)]  
+        )
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # 메모리 성장을 설정할 수 없는 경우
+        print(e)
